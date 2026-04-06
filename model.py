4a4b1d6d4aa7d0551e841b5b2f8610461640026b
Amisha103 <bhatiaamisha74@gmail.com>
2026-04-06 22:14:34 +0530

    project structure improved

--- model.py
+++ model.py
@@ -1,17 +1,51 @@
 import pandas as pd
-
+# 1. Load the dataset
 df = pd.read_csv("dataset/store_customers.csv")
-df = df.dropna()
+# 2. Data preprocessing
 # print(df.head())
 # print(df.info())
 # print(df.describe())
 # print(df.columns)
 # print(df.isnull().sum())
+df = df.dropna()
 df['Gender'] = df['Gender'].map({'M': 0, 'F': 1})
 
-X = df[['Gender', 'Age', 'Annual Income (k$)', 'Spending Score (1-100)']]
+X = df[['Gender', 'Age']]
 
+#Data Scaling
 from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
+# print(X_scaled)
+
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+wcss = []
+
+for i in range(1, 10):
+    kmeans = KMeans(n_clusters=i, random_state=42)
+    kmeans.fit(X_scaled)
+    wcss.append(kmeans.inertia_)
+
+# plt.plot(range(1, 10), wcss)
+# plt.xlabel("Number of clusters")
+# plt.ylabel("WCSS")
+# plt.title("Elbow Method")
+# plt.show()
+
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=5, random_state=42)
+labels = kmeans.fit_predict(X_scaled)
+
+
+df['Cluster'] = labels
+
+print(df.head())
+
+from sklearn.metrics import silhouette_score
+
+score = silhouette_score(X_scaled, labels)
+print("Silhouette Score:", score)
---
+++ visualisation.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import os
+
+# Create images folder if not exists
+os.makedirs("images", exist_ok=True)
+
+
+def plot_elbow(wcss):
+    plt.figure()
+    plt.plot(range(1, len(wcss)+1), wcss, marker='o')
+    plt.xlabel("Number of clusters")
+    plt.ylabel("WCSS")
+    plt.title("Elbow Method")
+
+    plt.savefig("images/elbow.png")
+    plt.show()
+
+
+def plot_feature_comparison(results_df):
+    plt.figure()
+    plt.bar(results_df["Features"], results_df["Silhouette Score"])
+    plt.xticks(rotation=45)
+    plt.xlabel("Feature Combinations")
+    plt.ylabel("Silhouette Score")
+    plt.title("Feature Comparison")
+
+    plt.tight_layout()
+    plt.savefig("images/feature_comparison.png")
+    plt.show()
+
+
+def plot_clusters(X_scaled, labels):
+    plt.figure()
+    plt.scatter(X_scaled[:, 0], X_scaled[:, 1], c=labels)
+    plt.xlabel("Feature 1")
+    plt.ylabel("Feature 2")
+    plt.title("Cluster Visualization")
+
+    plt.savefig("images/clusters.png")
+    plt.show()
